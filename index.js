8d403899ad25c1eb0fd70dabdc069fb83d6be505
Soham3905 <sohamdhakatecse3905@gmail.com>
2026-03-23 00:43:53 +0530

    server can send message to everyone

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-
+let userCount = 0;
+let allSockets = [];
 wss.on("connection", function (socket) {
-  console.log("User Connected..");
-  // setInterval(()=>{
-  //     socket.send("Current price of Solana : " + Math.random())
-  // },5000)
-  socket.on("message", (e) => {
-    // console.log(e.toString());
-    if (e.toString() === "ping") {
-      socket.send("pong");
-    }
+  userCount = userCount + 1;
+  allSockets.push(socket);
+  console.log("All Sockets Length " + allSockets.length);
+  console.log("User Connected #" + userCount);
+  socket.on("message", (message) => {
+    console.log("message received " + message.toString());
+    allSockets.forEach((socket) => {
+      socket.send(message.toString() + " sent from the server");
+    });
   });
+    socket.on("close", () => {
+      allSockets = allSockets.filter((x) => x !== socket);
+      console.log('User Disconnected');
+      console.log('Current allSockets length:', allSockets.length);
+  });
+  console.log(allSockets.length);
 });
